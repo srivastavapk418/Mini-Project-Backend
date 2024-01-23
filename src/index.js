46c553ce1cc511c26bf1046cd3e3d75f9b7ad726
srivastavapk418 <144578868+srivastavapk418@users.noreply.github.com>
2024-01-24 00:32:54 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const route = require("./route/route");
 const app = express();
+var cors = require("cors");
+app.use(cors());
 app.use(express.json());
 mongoose
   .connect(
